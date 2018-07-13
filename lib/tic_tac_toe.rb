86651f32f7f758dd0ab609c1f1d06a8b1c353670
Roberto Garza <robertg4567@hotmail.com>
2018-07-13 13:39:31 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,27 @@
+class toc_tac_toe
+
+  def initialize(board = [" "," "," "," "," "," "," "," "," "])
+    @board = board
+  end
+
+  def win_combinations = [
+    #Horizontal
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    #Vertical
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    #Diagonal
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end  
